0befcaaf907fb080848b8437548f0640ff2d9455
Andy Roulin <andy.roulin@gmail.com>
2019-07-10 22:59:45 -0700

    unix-passwd-dict-crack: document and refactor

--- python/unix-passwd-dict-crack.py
+++ python/unix-passwd-dict-crack.py
@@ -1,15 +1,27 @@
 import crypt
 import string
 
-def testPass(cryptPass):
+def testPass(cryptPass, dictFile):
+    """
+    Crack linux /etc/shadow password a dictionary file
+    :param str cryptPass: password in linux /etc/passwd format
+                          e.g., $6$salt$hashed_password
+    :param str dictFile: dictionary file
+
+    Prints the cracked password, if found, to std out
+    """
+
+    # password format: $num$salt$hashed_password
+    # fields          0  1   2      3
+
     if len(cryptPass.split('$')) < 4:
         return
-    salt = string.join(cryptPass.split('$')[0:3], '$')
-    dictFile = open('dictionary.txt', 'r')
-    for word in dictFile.readlines():
+
+    dict = open(dictFile, 'r')
+    crypt_salt = string.join(cryptPass.split('$')[0:3], '$')
+    for word in dict.readlines():
         word = word.strip('\n')
-        cryptWord = crypt.crypt(word, salt)
-        print cryptWord + " and " + cryptPass
+        cryptWord = crypt.crypt(word, crypt_salt)
         if (cryptWord == cryptPass):
             print "[+] Found Password: "+word+"\n"
             return
@@ -24,7 +36,7 @@ def main():
             user = line.split(':')[0]
             cryptPass = line.split(':')[1]
             print "[*] Cracking Password For: "+user
-            testPass(cryptPass)
+            testPass(cryptPass, "dictionary.txt")
 
 if __name__ == "__main__":
     main()
